9cc5417bf9923a0a2fadbd05486b42c7deb7143c
heartplay <168409591+heartplay@users.noreply.github.com>
2024-10-05 22:07:22 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,190 +1,191 @@
-const field = document.getElementById('field');
-// const elements = document.querySelectorAll('.element');
-const greenBtn = document.getElementById('green');
-const purpleBtn = document.getElementById('purple');
-const orangeBtn = document.getElementById('orange');
-const element1 = document.getElementById('element1');
-const element2 = document.getElementById('element2');
-const elements = [element1, element2];
-// const width = field.width;
-// const height = field.height;
-console.log(field.width);
+const field = document.getElementById('field')
+const elements = document.querySelectorAll('.element')
+const elementSize = 50
 
-// const xy1 = 'left:800px; top:500px;'
-// const xy2 = 'left:700px; top:700px;'\\
+let dx
+let dy
 
 
 
+let movedElement = null
+let selectedElement = null
+let mouseMoveHelper = null
+let otherElement = null
+let connectedElement = null
 
-// console.log(elements);
-let movedElement;
-let selectedElement;
-
-
-let isMoved = false;
-let isSelected = false;
-
-
-
+elements[1].style.left = `600px`
+elements[0].style.borderColor = `red`
+elements[1].style.borderColor = `blue`
 
 elements.forEach(element => {
-  element.addEventListener('click', (e) => {
+    element.style.width = `${elementSize}px`
+    element.style.height = `${elementSize}px`
+    element.style.zIndex = `500`   // zIndex используется только при наличии контекста наложения(элемент - корневой, элемент имеет position: absolute или relative, opacity < 1)
+    element.style.backgroundColor = `black`
+})
+
+let distanceConnection = Math.sqrt(elementSize ** 2 + elementSize ** 2)
+console.log(distanceConnection)
+
+
+// function getCoords(elem) {
+//     var coords = elem.getBoundingClientRect()
+//     return {
+//         x: coords.left,
+//         y: coords.top,
+//         r: coords.right,
+//         b: coords.bottom
+//     }
+// }
+// let coords = getCoords(elem)
+//     dx = mouseX - coords.x
+//     dy = mouseY - coords.y
+
+
+
+let distance = distanceBetween(elements[0], elements[1])
+// console.log('расстояние между элементами', distance)
+if (distance <= distanceConnection) {
+    console.log('connected')
+} else {
+    console.log('disconnected')
+}
+
+
+
+
+
+// console.log(`zindex 1 elem  ${elements[0].zIndex}, zindex 2 elem ${elements[1].zIndex}`)
+
+document.addEventListener('mousedown', (event) => {
+    const target = event.target.getAttribute("id");
+    if (target == "field" && selectedElement) {
+        selectedElement.style.borderStyle = `solid`
+        selectedElement = null
+        console.log('target', target, ' selectedElement',selectedElement)
+    }
+})
+
+elements.forEach(element => {
+    element.addEventListener('mousedown', event => mouseDown(event, element)) 
+})
+
+function mouseDown(e, elem) {
+    if (e.which !== 1) return
     if (selectedElement) {
-      selectedElement.style.borderStyle = 'solid';
-      selectedElement = null;
+        selectedElement.style.zIndex = `500`
+        selectedElement.style.borderStyle = 'solid'
+        selectedElement = null
     }
-    selectedElement = element;
-    // movedElement = element;
-    // selectedElement.style.top = element.style.top
+    selectedElement = elem
+    selectedElement.style.zIndex = `1000`
+    selectedElement.style.borderStyle = `dashed`
     
-    isSelected = true;
+    // console.log('elements ',elements)
+    // console.log('selectedElement ', selectedElement)
     
-    selectedElement.style.borderStyle = 'dashed';
-    
+    movedElement = elem
+    elements.forEach(element => {
+        if (element !== movedElement) {
+            otherElement = element
+        }
+    })
+    // console.log('неподвижный элемент', otherElement)
+    // console.log('выбранный ', selectedElement)
+    // console.log('передвигаемый ', movedElement)
 
-    // selectedElement.style = selectedElement.style + 'border-style: dashed;';
-    // console.log(selectedElement.style.top);
-    // console.log(elements);
+    movedElement.ondragstart = function() {
+        return false;
+      };
     
-  });
-  element.addEventListener('mousedown', (e) => {
-    movedElement = element;
-    // selectedElement = element;
-    
-   
-    document.addEventListener('mousemove', move);
-    
-  });
-    document.addEventListener('mouseup', mouseUp);
-});
-// function mouseDown() {
-//   document.addEventListener('mousemove', move)
-// }
+    var mouseX = e.clientX
+    var mouseY = e.clientY
+    let coords = getCoords(elem)
+    dx = mouseX - coords.x
+    dy = mouseY - coords.y
+    mouseMoveHelper = event => mouseMove(event, movedElement)
+    document.addEventListener('mousemove', mouseMoveHelper)
+    document.addEventListener('mouseup', mouseUp)
+
+    // console.log('кнопка зажата, выбранный ', selectedElement)
+    // console.log('кнопка зажата, передвигаемый', movedElement)
+}
+
+function mouseMove(event, movedElement) {
+    elements.forEach(element => {
+        if (element !== movedElement) {
+            otherElement = element
+        }
+    })
+
+    // console.log('неподвижный элемент при mouseMove', otherElement)
+    var mouseX = event.clientX 
+    var mouseY = event.clientY
+    let fieldCoords = getCoords(field)
+    let x = mouseX - dx
+    let y = mouseY - dy
+    if (x <= fieldCoords.x) {
+        x = fieldCoords.x
+    }
+    if (x + elementSize >= fieldCoords.r) {
+        x = fieldCoords.r - elementSize
+    }
+    if (y <= fieldCoords.y) {
+        y = fieldCoords.y
+    }
+    if (y + elementSize >= fieldCoords.b - 1) {
+        y = fieldCoords.b - elementSize
+    } 
+    movedElement.style.left = `${x - fieldCoords.x}px`
+    movedElement.style.top = `${y - fieldCoords.y}px`
+
+    let distance = distanceBetween(movedElement, otherElement)
+    console.log('расстояние между элементами', distance)
+    if (distance <= distanceConnection) {
+    console.log('connected')
+    connectedElement = otherElement
+    console.log(connectedElement)
+    } else {
+    console.log('disconnected')
+    connectedElement = null
+    console.log(connectedElement)
+    }
+
+    // console.log('движение, выбранный ', selectedElement)
+    // console.log('движение, передвигаемый', movedElement)
+}
+
 function mouseUp() {
-  document.removeEventListener('mousemove', move);
-  
-}
-function move(event) {
-    // let x = event.clientX;
-    // let y = event.clientY;
-    let x = `${event.clientX - 25}px`;
-    let y = `${event.clientY - 25}px`;
-
-    let movingArea = field.getBoundingClientRect();
-    // console.log(field.getBoundingClientRect().);
-    if (event.clientX <= movingArea.left) {
-      x = movingArea.left;
+    if (mouseMoveHelper) {
+        document.removeEventListener('mousemove', mouseMoveHelper);
+        mouseMoveHelper = null
     }
-    if (event.clientY <= movingArea.top) {
-      y = movingArea.top;
+    if (movedElement) {
+        movedElement = null
     }
-    if (event.clientX >= movingArea.right) {
-      x = movingArea.right;
+    document.removeEventListener('mouseup', mouseUp)
+    
+    // console.log('кнопка отпущена, выбранный ', selectedElement)
+    // console.log('кнопка отпущена, передвигаемый', movedElement)
+}
+
+function getCoords(elem) {
+    var coords = elem.getBoundingClientRect()
+    return {
+        x: coords.left,
+        y: coords.top,
+        r: coords.right,
+        b: coords.bottom
     }
-    if (event.clientY >= movingArea.bottom) {
-      y = movingArea.bottom;
-    }
-    // if (event.clientX <= movingArea.left || event.clientX >= movingArea.left + field.width) {
-    //   x = movingArea.left;
-    // }
-    // if (event.clientY <= movingArea.top || event.clientY >= movingArea.top + field.height) {
-    //   y = movingArea.top;
-    // }
-    movedElement.style.left = x;
-    movedElement.style.top = y;
-
-
-    
-
-
-    
-    // let movingArea = field.getBoundingClientRect();
-    // if (x <= movingArea.left) {
-    //   event.clientX = movingArea.left;
-    // }
-    // if (event.clientY <= movingArea.top) {
-    //   event.clientY = movingArea.top;
-    // }
-    // movedElement.style.left = `${event.clientX - 25}px`;
-    // movedElement.style.top = `${event.clientY - 25}px`;
-
-
-}
-    
-greenBtn.onclick = function () {
-  if (selectedElement) {
-    selectedElement.className = 'element-green';
-  }  
-  
 }
 
-purpleBtn.onclick = function () {  
-  if (selectedElement) {
-    selectedElement.className = 'element-purple';
-  }
+function distanceBetween(elem1, elem2) {
+    var coords1 = getCoords(elem1)
+    var coords2 = getCoords(elem2)
+    return Math.sqrt((coords1.x - coords2.x) ** 2 + (coords1.y - coords2.y) ** 2)
 }
 
-orangeBtn.onclick = function () {  
-  if (selectedElement) {
-    selectedElement.className = 'element-orange';
-  };
-}
+// function moveConnectedElement(movedElement, connectedElement) {
 
-// greenBtn.onclick = function () {  
-//   selectedElement.style = 'background-color: rgba(62, 184, 171, 0.3)';
+
 // }
-
-// function move(event) {
-//   if (isMoved) {
-//     movedElement.style.left = event.clientX + 'px';
-//     movedElement.style.top = event.clientY + 'px';
-//   }
-    
-// }
-
-// function stop() {
-//   document.removeEventListener('mousemove', move());
-//   isMoved = false;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// element1.addEventListener('mousedown', onMouseDown);
-// element1.addEventListener('mouseup', onMouseUp);
-
-
-
-
-// ПЕРЕМЕЩЕНИЕ
-
-// function onMouseDown() {
-//   document.addEventListener('mousemove', onMouseMove);
-// }
-// function onMouseUp() {
-//   document.removeEventListener('mousemove', onMouseMove);
-// }
-// function onMouseMove(event) {
-//   element1.style.left = event.clientX + 'px';
-//   element1.style.top = event.clientY + 'px';
-// }
-
-
-
-
-
